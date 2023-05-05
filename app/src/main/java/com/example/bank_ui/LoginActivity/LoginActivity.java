150cfdac78bf319f285612fc9c5a3787c99eaec7
Shubham <agehlot806@gmail.com>
2023-05-05 14:59:40 +0530

    firebase

--- app/src/main/java/com/example/bank_ui/LoginActivity/LoginActivity.java
+++ app/src/main/java/com/example/bank_ui/LoginActivity/LoginActivity.java
@@ -2,24 +2,41 @@ package com.example.bank_ui.LoginActivity;
 
 import static android.os.Build.VERSION_CODES.R;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.bank_ui.BottomSheets.OtpVerification;
 import com.example.bank_ui.MainActivity;
 import com.example.bank_ui.WelcomeActivity.SliderActivity;
 import com.example.bank_ui.WelcomeActivity.Splash;
 import com.example.bank_ui.databinding.ActivityLoginBinding;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class LoginActivity extends AppCompatActivity {
     ActivityLoginBinding binding;
     Intent intent;
+    FirebaseAuth mAuth;
+    private static final String TAG = "LoginActivity";
+
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,41 +44,61 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth = FirebaseAuth.getInstance();
+
         binding.clickForSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(LoginActivity.this, Signup.class);
+//                intent.putExtra("mobile",binding.mobileEt.getText().toString());
                 startActivity(intent);
                 finish();
             }
         });
 
-
-        binding.mainLayout.setBackgroundColor(Color.WHITE);
-        binding.scroll.setVisibility(View.GONE);
-        binding.scroll.setVisibility(View.GONE);
-        binding.introAnimation.setVisibility(View.VISIBLE);
         binding.loginBtn.setOnClickListener(view -> {
 
-            OtpVerification otpFragment = new OtpVerification();
-            otpFragment.show(getSupportFragmentManager(), otpFragment.getTag());
+//            OtpVerification otpFragment = new OtpVerification();
+//            otpFragment.show(getSupportFragmentManager(), otpFragment.getTag());
+//            intent.putExtra("mobile", binding.mobileEt.getText().toString());
+            sendotp();
         });
     }
 
-    @Override
+    private void sendotp() {
 
-    protected void onResume() {
-        super.onResume();
-        new Handler().postDelayed(new Runnable() {
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
             @Override
-            public void run() {
-                binding.mainLayout.setBackgroundColor(Color.BLUE);
-                binding.scroll.setVisibility(View.VISIBLE);
-                binding.scroll.setVisibility(View.VISIBLE);
-                binding.introAnimation.setVisibility(View.GONE);
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
             }
 
-        }, 5000);
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
 
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                Bundle bundle = new Bundle();
+                bundle.putString("verificationId", verificationId);
+                OtpVerification otpFragment = new OtpVerification();
+                otpFragment.setArguments(bundle);
+                Toast.makeText(getApplicationContext(),"OTP Send Successfully...", Toast.LENGTH_SHORT).show();
+                otpFragment.show(getSupportFragmentManager(), otpFragment.getTag());
+
+            }
+        };
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber("+91" + binding.mobileEt.getText().toString().trim())       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // (optional) Activity for callback binding
+                        // If no activity is passed, reCAPTCHA verification can not be used.
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 }
