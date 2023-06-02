cea3a6c34e70396da5a68e80c1abf69418ea531c
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-06-02 12:55:36 +0530

    firebase

--- app/src/main/java/com/example/bank_ui/CreditActivity.java
+++ app/src/main/java/com/example/bank_ui/CreditActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.bank_ui.Adapter.CreditCardAdapter;
@@ -26,6 +28,8 @@ public class CreditActivity extends AppCompatActivity {
     ApiInterface apiInterface;
     String from;
     Call<List<CreditCardResponse>> call;
+
+    private static final String TAG = "CreditActivityLOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,22 +57,29 @@ public class CreditActivity extends AppCompatActivity {
 
 
         }
+
+        binding.loadingCard.setVisibility(View.VISIBLE);
         call.enqueue(new Callback<List<CreditCardResponse>>() {
             @Override
             public void onResponse(Call<List<CreditCardResponse>> call, Response<List<CreditCardResponse>> response) {
                 if (response.isSuccessful()) {
+                    binding.loadingCard.setVisibility(View.GONE);
                     List<CreditCardResponse> res = response.body();
                     binding.rCreditCard.setLayoutManager(new LinearLayoutManager(CreditActivity.this));
                     binding.rCreditCard.setAdapter(new CreditCardAdapter(CreditActivity.this, res, from));
 
 
                 } else {
+                    Log.d(TAG, response.message());
+                    binding.loadingCard.setVisibility(View.GONE);
                     Toast.makeText(CreditActivity.this, "try again", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<List<CreditCardResponse>> call, Throwable t) {
+                binding.loadingCard.setVisibility(View.GONE);
+                Log.d(TAG, t.getMessage());
                 Toast.makeText(CreditActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
