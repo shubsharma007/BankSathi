7885045efb31f4394adb51a240706e3714ba0acf
yogesh gurjar <yogesh67gurjar@gmail.com>
2023-06-05 17:02:23 +0530

    api integration

--- app/src/main/java/com/example/bank_ui/Fragment/MyStatsFragment.java
+++ app/src/main/java/com/example/bank_ui/Fragment/MyStatsFragment.java
@@ -3,22 +3,27 @@ package com.example.bank_ui.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bank_ui.Adapter.CustomerAdapter;
 import com.example.bank_ui.R;
 import com.example.bank_ui.databinding.FragmentMyStatsBinding;
 
 public class MyStatsFragment extends Fragment {
     FragmentMyStatsBinding binding;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding =FragmentMyStatsBinding.inflate(inflater,container,false);
+        binding = FragmentMyStatsBinding.inflate(inflater, container, false);
 
-
+        binding.rvAddCustomer.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvAddCustomer.setAdapter(new CustomerAdapter(getContext()));
 
         return binding.getRoot();
     }
